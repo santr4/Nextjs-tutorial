5a2bb627b58cf12af5ffa93403d7adf17998d16f
Aaranyak Santra <santra.aaranyak16092003@gmail.com>
2024-01-14 16:27:22 +0530

    [add] : clsx to toggle with conditional class names

--- nextjs-dashboard/app/ui/invoices/status.tsx
+++ nextjs-dashboard/app/ui/invoices/status.tsx
@@ -1,7 +1,7 @@
 import { CheckIcon, ClockIcon } from '@heroicons/react/24/outline';
-import clsx from 'clsx';
+import { clsx } from 'clsx';  {/* this is to toggle with the class names*/}
 
-export default function InvoiceStatus({ status }: { status: string }) {
+function InvoiceStatus({ status }: { status: string }) {
   return (
     <span
       className={clsx(
@@ -27,3 +27,5 @@ export default function InvoiceStatus({ status }: { status: string }) {
     </span>
   );
 }
+
+export default InvoiceStatus;
